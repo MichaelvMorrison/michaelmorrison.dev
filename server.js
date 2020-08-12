d37cb2aa2a6e583da923844347dd48d096a5eaef
Michael Morrison <morrim5@rpi.edu>
2020-08-12 00:49:57 -0400

    init

---
+++ server.js
@@ -0,0 +1,9 @@
+/*jshint esversion: 6 */
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => console.log(`\nlistening on port: ${port}`));
+
+app.use(express.static('public'));
