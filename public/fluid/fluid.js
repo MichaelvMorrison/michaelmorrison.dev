292004b3e8916b720585cedb62f67eb0ab9e3c3a
Michael Morrison <morrim5@rpi.edu>
2021-04-23 04:22:00 -0400

    .

---
+++ public/fluid/fluid.js
@@ -0,0 +1,68 @@
+class Fluid {
+  constructor(dt, diffusion, viscosity){
+    this.size = N;
+    this.dt = dt;
+    this.diff = diffusion;
+    this.visc = viscosity;
+
+    this.s = new Array(N * M).fill(0);
+    this.density = new Array(N * M).fill(0);
+
+    this.Vx = new Array(N * M).fill(0);
+    this.Vy = new Array(N * M).fill(0);
+
+    this.Vx0 = new Array(N * M).fill(0);
+    this.Vy0 = new Array(N * M).fill(0);
+  }
+
+  addDensity(x, y, amount){
+    let index = IX(x, y);
+    this.density[index] += amount;
+  }
+
+  addVelocity(x, y, amountX, amountY){
+    let index = IX(x, y);
+    this.Vx[index] += amountX;
+    this.Vy[index] += amountY;
+  }
+
+  renderDensity(){
+    noStroke();
+    for (let i = 0; i < N; i++){
+      for (let j = 0; j < M; j++){
+        let x = i * SCALE;
+        let y = j * SCALE;
+        let d = this.density[IX(i,j)];
+        colorMode(HSB);
+        fill(hue_offset + (t*50) % 360, 100, d);
+        rect(x, y, SCALE, SCALE);
+      }
+    }
+  }
+
+  step(){
+    let visc    = this.visc;
+    let diff    = this.diff;
+    let dt      = this.dt;
+    let Vx      = this.Vx;
+    let Vy      = this.Vy;
+    let Vx0     = this.Vx0;
+    let Vy0     = this.Vy0;
+    let s       = this.s;
+    let density = this.density;
+    
+    diffuse(1, Vx0, Vx, visc, dt);
+    diffuse(2, Vy0, Vy, visc, dt);
+    
+    project(Vx0, Vy0, Vx, Vy);
+    
+    advect(1, Vx, Vx0, Vx0, Vy0, dt);
+    advect(2, Vy, Vy0, Vx0, Vy0, dt);
+    
+    project(Vx, Vy, Vx0, Vy0);
+    
+    diffuse(0, s, density, diff, dt);
+    advect(0, density, s, Vx, Vy, dt);
+  }
+}
+
---
+++ public/fluid/js.js
@@ -0,0 +1,47 @@
+const SCALE = 10;
+const ITER = 4;
+
+let N = Math.ceil(window.innerWidth / SCALE);
+let M = Math.ceil(window.innerHeight / SCALE);
+
+let t = 0;
+let hue_offset;
+
+let cx_prev = 0;
+let cy_prev = 0;
+
+let cx = 0;
+let cy = 0;
+
+let fluid;
+
+function setup() {
+  hue_offset = random(0,360);
+  background(0);
+  createCanvas(window.innerWidth, window.innerHeight);
+  frameRate(60);
+  fluid = new Fluid(0.5, 0.0000001, 0);
+}
+
+function draw() {
+  background(0);
+
+  cx_prev = cx;
+  cy_prev = cy;
+
+  cx = int(mouseX / SCALE);
+  cy = int(mouseY / SCALE);
+  fluid.addDensity(cx, cy, 600);
+
+  t += 0.01;
+  fluid.addVelocity(cx, cy, cx-cx_prev, cy-cy_prev);
+
+  fluid.step();
+  fluid.renderDensity();
+}
+
+function windowResized(){
+  resizeCanvas(window.innerWidth, window.innerHeight);
+  N = Math.ceil(window.innerWidth / SCALE);
+  M = Math.ceil(window.innerHeight / SCALE);
+}
